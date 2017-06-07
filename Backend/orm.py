f36c21a1730c23d62a0887dca78ad8d36532635e
John Espenhahn <john@espenhahn.org>
2017-06-07 14:37:53 -0400

    Finish ORM

--- Backend/orm.py
+++ Backend/orm.py
@@ -1,23 +1,34 @@
 from peewee import *
 
-class Users(Model):
+password = raw_input("Database password: ")
+db = MySQLDatabase('carbondb', host='carboncoderhackathon.caqu9lasjuwn.us-east-1.rds.amazonaws.com', user='zhangster', passwd=password)
+
+class BaseModel(Model):
+	class Meta:
+		database=db
+
+class Users(BaseModel):
 	uid = PrimaryKeyField()
 
-class Tag(Model):
+class Tag(BaseModel):
 	tid = PrimaryKeyField()
 	tag_index = IntegerField()
 	tag_text = CharField()
 	percent = IntegerField()
 
-class Picture(Model):
+class Picture(BaseModel):
 	pid = PrimaryKeyField()
 	uid = ForeignKeyField(Users)
 	latitude = DoubleField()
 	longitude = DoubleField()
 
-class PicTags(Model):
+class PicTags(BaseModel):
 	pid = ForeignKeyField(Picture)
 	tid = ForeignKeyField(Tag)
 
 	class Meta:
 		primary_key = CompositeKey('pid','tid')
+
+db.connect()
+
+# TODO do stuff
