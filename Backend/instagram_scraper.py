a2d6af37b585ff3a626adbffa3d04a1b9c35060b
Brandt Welch <bewelch21@gmail.com>
2017-06-07 21:19:47 -0400

    Location scraping added

--- Backend/instagram_scraper.py
+++ Backend/instagram_scraper.py
@@ -3,6 +3,21 @@ import urllib
 import json 
 import sys
 
+def scrape_location_data(username, img_code):
+	h = httplib2.Http()
+	resp, content = h.request("http://www.instagram.com/p/%s/?taken-by=%s" % (img_code, username))
+	beg_index = content.find(b"window._sharedData = ")
+	end_index = content.find(b";</script>")
+	trimmed_content = content[beg_index:end_index]
+	trimmed_content = trimmed_content.replace(b"window._sharedData = ",b"")
+
+	insta_array = json.loads(trimmed_content, encoding='utf-8')
+	media = insta_array['entry_data']['PostPage'][0]['graphql']['shortcode_media']['location']
+	if media:
+		return str(media['name'])
+	else:
+		return None
+
 def scrape_insta(username):
 	h = httplib2.Http()
 	resp, content = h.request("http://instagram.com/%s" % username)
@@ -22,8 +37,8 @@ def scrape_insta(username):
 		if media['is_video']: # Don't include videos
 			pass
 		else:
-			img_array.append(str(media['display_src']))
+			location = scrape_location_data(username, media['code'])
+			img_array.append((str(media['display_src']), location))
 		i += 1
 
 	return img_array
-	
